8de79ac5f552ede0cd7c0646dfb0a077ff6e1c8c
mohinigurjar <gurjarmohini112@gmail.com>
2025-09-24 18:07:06 +0530

    optimized

--- Heaps/merge-k-sorted-arrays.cpp
+++ Heaps/merge-k-sorted-arrays.cpp
@@ -26,3 +26,46 @@ class Solution {
         return ans;
     }
 };
+
+
+
+
+
+
+/// ------ optimized solution ------
+// Time: O(N log K)
+
+class Solution {
+  public:
+    // Function to merge k sorted arrays.
+    vector<int> mergeKArrays(vector<vector<int>> arr, int k) {
+        //min heap
+        vector<pair<int, pair<int, int>>> temp;
+        
+        for(int i=0; i<k; i++){
+            temp.push_back(make_pair(arr[i][0], make_pair(i, 0)));
+        }
+       //store the element and the row and column of that element
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> p(temp.begin(), temp.end());
+        
+        
+        vector<int> ans;
+        pair<int, pair<int, int>> Element;
+        
+        int i, j;
+        
+        while(!p.empty()){
+            Element = p.top();
+            p.pop();
+            ans.push_back(Element.first);
+            i = Element.second.first;
+            j = Element.second.second;
+            if(j+1 < k){
+                p.push(make_pair(arr[i][j+1], make_pair(i, j+1)));
+            }
+        }
+        
+        return ans;
+        
+    }
+};
